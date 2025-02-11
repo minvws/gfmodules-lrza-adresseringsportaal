37df1717880b75bc8e1964f7527110aa621deb85
Nick ten Cate <nick@nicktencate.com>
2025-02-11 13:02:10 +0100

    Allow TLS for database connection

--- config/database.php
+++ config/database.php
@@ -77,7 +77,10 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => env('DB_APPLICATION_SSLMODE', 'prefer'),
+            'sslrootcert' => env('DB_APPLICATION_SSLROOTCERT', null),
+            'sslcert' => env('DB_APPLICATION_SSLCERT', null),
+            'sslkey' => env('DB_APPLICATION_SSLKEY', null),
         ],
 
         'sqlsrv' => [
